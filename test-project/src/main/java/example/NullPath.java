29d13183e94957dcb448099848967fe3b16dac5f
Peter Hegedus <hpeter@inf.u-szeged.hu>
2022-02-15 14:48:34 +0100

    Minor fixes.

--- test-project/src/main/java/example/NullPath.java
+++ test-project/src/main/java/example/NullPath.java
@@ -1,6 +1,6 @@
 package example;
 
-class ullPath {
+class NullPath {
     public static int foo(String str) {
         String s = str.intern(); //str could be NULL, so NP_NULL_ON_SOME_PATH should occur
         for (int i=0; i < str.length(); i++) {
